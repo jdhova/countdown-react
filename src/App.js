4bef0bee982f2aac30ff40c671e5253248669b7b
Jude Okagu <juud0010@gmail.com>
2021-05-27 23:05:55 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <section className='main-timer'>
+      <section className='timer-1'>
+        <div>
+          <h2>Count down to my First $1M</h2>
+        </div>
+      </section>
+
+      <section className='timer-2'>
+        <section className='min-timer'>
+          <h4>88</h4>
+
+          <h4>
+            <small>Years</small>
+          </h4>
+        </section>
+        <h4>
+          <span>:</span>
+        </h4>
+
+        <section className='min-timer'>
+          <h4>88</h4>
+          <h4>
+            <small>Months</small>
+          </h4>
+        </section>
+        <h4>
+          <span>:</span>
+        </h4>
+        <section className='min-timer'>
+          <h4>88</h4>
+          <h4>
+            <small>Weeks</small>
+          </h4>
+        </section>
+        <h4>
+          <span>:</span>
+        </h4>
+        <section className='min-timer'>
+          <h4>88</h4>
+          <h4>
+            <small>Days</small>
+          </h4>
+        </section>
+        <section className='min-timer'>
+          <h4>88</h4>
+          <h4>
+            <small>Hours</small>
+          </h4>
+        </section>
+        <section className='min-timer'>
+          <h4>88</h4>
+          <h4>
+            <small>Mins</small>
+          </h4>
+        </section>
+        <section className='min-timer'>
+          <h4>88</h4>
+          <h4>
+            <small>Seconds</small>
+          </h4>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default App;
